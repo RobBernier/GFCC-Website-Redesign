e06d8aaaa8051b88a7b1de874b9026e60615ced8
rbernier93 <robertbernier93@gmail.com>
2016-11-22 10:37:28 -0500

    added more automation, including css and js automation

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,29 @@
 var gulp = require('gulp'),
     watch = require('gulp-watch'),
+    sass = require('gulp-sass'),
     browserSync = require('browser-sync').create();
 
 gulp.task('html', function() {
-    gulp.src(["./*.html", "./*.php"])
-        .pipe(gulp.dest('dist/html'))
+    gulp.src(["./html/*.html", "./*.php"])
+        .pipe(gulp.dest('./dist/html'))
         .pipe(browserSync.stream());
 });
 
 gulp.task('css', function() {
-    gulp.src(["./*.css"])
-        .pipe(gulp.dest('dist/css'))
+    gulp.src(["./css/*.css"])
+        .pipe(gulp.dest('./dist/css'))
         .pipe(browserSync.stream());
 });
 
+gulp.task('sass', function () {
+  return gulp.src('./css/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./dist/css'));
+});
+
 gulp.task('js', function() {
-    gulp.src(["./*.js"])
-        .pipe(gulp.dest('dist/js'))
+    gulp.src(["./js/*.js"])
+        .pipe(gulp.dest('./dist/js'))
         .pipe(browserSync.stream());
 });
 
@@ -30,6 +37,7 @@ gulp.task('browser-sync', function() {
 
 gulp.watch(["html/*.html"], ['html']);
 gulp.watch(["css/*.css"], ['css']);
+gulp.watch(["css/*.scss"], ['sass']);
 gulp.watch(["js/*.js"], ['js']);
 //gulp.watch(["src/**/*.html", "src/**/*.php"], ['browser-sync']);
 //gulp.watch(["src/js/**/*.js"], ['browser-sync']);
