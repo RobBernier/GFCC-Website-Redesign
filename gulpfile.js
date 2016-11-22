11d991d2aff2fd668b46135a789c339325e0389c
rbernier93 <robertbernier93@gmail.com>
2016-11-22 16:28:30 -0500

    added more automation including concatination of .css files as well as sass configurations

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,33 @@
 var gulp = require('gulp'),
     watch = require('gulp-watch'),
     sass = require('gulp-sass'),
-    browserSync = require('browser-sync').create();
+    concat = require('gulp-concat'),
+    minifyCSS = require('gulp-clean-css'),
+    sitemap = require('gulp-sitemap'),
+    browserSync = require('browser-sync');
 
 gulp.task('html', function() {
-    gulp.src(["./html/*.html", "./*.php"])
+    gulp.src(["./_src/html/*.html"])
         .pipe(gulp.dest('./dist/html'))
         .pipe(browserSync.stream());
 });
 
 gulp.task('css', function() {
-    gulp.src(["./css/*.css"])
+  return gulp.src(["./_src/css/*.css"])
+        .pipe(concat('main.css'))
         .pipe(gulp.dest('./dist/css'))
         .pipe(browserSync.stream());
 });
 
 gulp.task('sass', function () {
-  return gulp.src('./css/*.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./dist/css'));
+    return gulp.src("./_src/css_scss/*.scss")
+        .pipe(sass())
+        .pipe(gulp.dest("./dist/css"))
+        .pipe(browserSync.stream());
 });
 
 gulp.task('js', function() {
-    gulp.src(["./js/*.js"])
+    gulp.src(["./_src/js/*.js"])
         .pipe(gulp.dest('./dist/js'))
         .pipe(browserSync.stream());
 });
@@ -35,12 +40,12 @@ gulp.task('browser-sync', function() {
     });
 });
 
-gulp.watch(["html/*.html"], ['html']);
-gulp.watch(["css/*.css"], ['css']);
-gulp.watch(["css/*.scss"], ['sass']);
-gulp.watch(["js/*.js"], ['js']);
-//gulp.watch(["src/**/*.html", "src/**/*.php"], ['browser-sync']);
-//gulp.watch(["src/js/**/*.js"], ['browser-sync']);
+//Watches files and runs command respectfully
+gulp.watch(["./_src/html/*.html"], ['html']);
+gulp.watch(["./_src/css/*.css"], ['css']);
+gulp.watch(["./_src/css_scss/*.scss"], ['sass','css']);
+gulp.watch(["./_src/js/*.js"], ['js']);
+
 
 //Running 'gulp' from the command line will run all related tasks.
 gulp.task('default', ['browser-sync']);
